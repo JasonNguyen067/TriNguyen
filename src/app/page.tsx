dc74ad12d50dad4ac5664cda15d1d988544e0adf
JasonNguyen067 <jasonnguyen7506@gmail.com>
2026-08-04 19:38:53 -0500

    feat: hero page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,11 @@
 import Hero from "@/components/sections/hero";
+import Moments from "@/components/sections/Moments";
 
 export default function Home() {
   return (
-    <Hero />
+    <>
+      <Hero />
+      <Moments />
+    </>
   );
 }
